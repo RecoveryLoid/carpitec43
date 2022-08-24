9c6040c2895fcd388e4ada04b2abb66b3a927ffd
cris <cristiancala@protonmail.com>
2022-08-24 02:41:01 -0400

    [js, html]Agregada primera validacion issue-3

--- js/validation.js
+++ js/validation.js
@@ -1,3 +1,10 @@
+function showError(className ,errorText) {
+    const warning = document.getElementById("warning");
+    warning.innerHTML = errorText;
+    warning.classList.remove('d-none');
+    warning.classList.add(className);
+}
+
 function validateForm() {
     const regex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
     const disabledBtn = document.getElementById('submit').disabled=false;
@@ -6,11 +13,7 @@ function validateForm() {
     var message = document.getElementById('message').value;
 
     if ([name, email, message].includes("")) {
-        document.getElementById("warning").innerHTML = "Todos los campos son obligatorios";
+        showError('bg-danger','Todos los campos son obligatorios');
         return false;
     }
-}
-
-function showError(error, message) {
-    const warning = document.getElementById("warning");
 }
